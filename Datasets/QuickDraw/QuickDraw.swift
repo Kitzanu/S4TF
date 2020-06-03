c840a0ccd9b318902f6f2b1475b244a6cd84ee03
Kitzanu <65956534+Kitzanu@users.noreply.github.com>
2020-06-03 17:58:02 +0200

    Update QuickDraw.swift

--- Datasets/QuickDraw/QuickDraw.swift
+++ Datasets/QuickDraw/QuickDraw.swift
@@ -86,7 +86,7 @@ public struct QuickDraw<Entropy: RandomNumberGenerator> {
         remoteBaseDirectory: "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap",
         //imagesFilename:  String(tfds.features.Image(shape:_QUICKDRAW_IMAGE_SHAPE))!,
         imagesFilename: "airplane",
-        labelsFilename: "image_classification/quickdraw_labels.txt"
+        labelsFilename: "image_classification/quickdraw_labels.txt"),
         //labelsFilename:  String(tfds.features.ClassLabel(names_file:tfds.core.get_tfds_path(_QUICKDRAW_LABELS_FNAME)))!
       batchSize: batchSize, entropy: entropy
     ).lazy.map { (batches: Batches) -> LazyMapSequence<Batches, LabeledImage> in
