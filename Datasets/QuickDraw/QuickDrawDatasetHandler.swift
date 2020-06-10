d3af06578863cad1f5cd322bf236648d416af74b
Kitzanu <65956534+Kitzanu@users.noreply.github.com>
2020-06-10 13:14:49 +0200

    Update QuickDrawDatasetHandler.swift

--- Datasets/QuickDraw/QuickDrawDatasetHandler.swift
+++ Datasets/QuickDraw/QuickDrawDatasetHandler.swift
@@ -15,12 +15,14 @@ func fetchQuickDrawDataset(
 
   let imagesData = DatasetUtilities.fetchResource(
     filename: imagesFilename,
-    fileExtension: "npz",
+    //fileExtension: "npz",
+    fileExtension: "gz",
     remoteRoot: remoteRoot,
     localStorageDirectory: localStorageDirectory)
   let labelsData = DatasetUtilities.fetchResource(
     filename: labelsFilename,
-    fileExtension: "npz",
+    //fileExtension: "npz",
+    fileExtension: "gz",
     remoteRoot: remoteRoot,
     localStorageDirectory: localStorageDirectory)
 
